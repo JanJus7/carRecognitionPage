cb6d009ceba255bdd2b11791f2c47d2b88893a4e
Jan <janjus2004@gmail.com>
2025-06-14 16:53:48 +0200

    built first vite template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-2xl">
+      Tailwind działa
+    </div>
+  )
+}
+
+export default App;
