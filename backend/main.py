696949154ace9ec24f11b72e9c5c29f26a81dbda
Jan <janjus2004@gmail.com>
2025-06-14 20:46:47 +0200

    testing mongo and it works

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 import redis
 
@@ -8,16 +8,31 @@ mongo_client = MongoClient("mongodb://mongo:27017")
 mongo_db = mongo_client["carx_db"]
 print("Połączono z MongoDB")
 
-redis_client = redis.Redis(host='redis', port=6379)
+redis_client = redis.Redis(host="redis", port=6379)
 try:
     redis_client.ping()
     print("Połączono z Redis")
 except redis.exceptions.ConnectionError:
     print("Redis nieosiągalny")
 
-@app.route("/ping")
-def ping():
-    return jsonify({"status": "ok"})
+
+@app.route("/photos", methods=["POST"])
+def add_photo():
+    try:
+        data = request.json
+        if not data:
+            return jsonify({"error": "Brak danych"}), 400
+        mongo_db.photos.insert_one(data)
+        return jsonify({"message": "Dodano zdjęcie"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/photos", methods=["GET"])
+def get_photos():
+    photos = list(mongo_db.photos.find({}, {"_id": 0}))
+    return jsonify(photos)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
